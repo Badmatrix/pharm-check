5e448b32b78bd5579c38b79916ac5feb4345b846
princedev3 <marvinprince232@gmail.com>
2024-08-13 12:40:44 +0300

    changes

--- src/ui/SideBar.jsx
+++ src/ui/SideBar.jsx
@@ -1,3 +1,31 @@
+import { useState } from "react";
+import { useEffect } from "react";
+import { useRef } from "react";
+
+
 export default function SideBar() {
-  return <div className=" col-span-1 ">SideBar ss</div>;
+
+  const divRef = useRef()
+  const[height,setHeight]=useState(0)
+  const[open,setOpen]=useState(false)
+
+ 
+  useEffect(()=>{
+    setHeight(divRef.current.scrollHeight);
+  },[open,height,divRef])
+
+  return <div className=" col-span-1 ">
+<div className="">
+  <button onClick={()=>setOpen(!open)}>show more</button>
+  <div ref={divRef} style={{height:open?`${height}px`:"0px"}} className="overflow-hidden transition-all duration-300">
+    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quam, excepturi?
+  </div>
+  <div className="font-bold">
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil, explicabo.
+  </div>
+</div>
+
+      
+
+  </div>;
 }
