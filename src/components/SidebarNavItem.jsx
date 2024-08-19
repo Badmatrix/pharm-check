cea0f5f109908719563d83082ab86d80ca10d933
princedev3 <marvinprince232@gmail.com>
2024-08-20 00:45:59 +0300

    animation toggle

--- src/components/SidebarNavItem.jsx
+++ src/components/SidebarNavItem.jsx
@@ -1,12 +1,38 @@
+import { useEffect, useRef, useState } from "react";
 import { FaChevronDown, FaChevronUp } from "react-icons/fa";
 import { Link } from "react-router-dom";
 
-export default function SidebarNavItem({ item, activeSubMenu, toggleSubMenu, index }) {
+
+
+
+export default function SidebarNavItem({ item, index, setOpen, open, activeIndex, setActiveIndex }) {
+  const devRef = useRef(null);
+  const [heights, setHeights] = useState(0);
+
+  useEffect(() => {
+    if (devRef.current) {
+      setHeights(devRef.current.scrollHeight);
+    }
+  
+  }, [open, index, setActiveIndex]);
+
+  const handleToggle = () => {
+    if (activeIndex === index) {
+      setOpen(!open);
+
+    } else {
+      setOpen(true);
+      setActiveIndex(index);
+    
+    }
+  };
+
+
   return (
-    <li>
+    <div key={index}>
       <button
+        onClick={handleToggle}
         className="text-white w-full text-left py-1 px-3 flex justify-between items-center"
-        onClick={() => toggleSubMenu(index)}
       >
         <span className="flex items-center">
           {item.icon} {/* Icon displayed here */}
@@ -14,20 +40,87 @@ export default function SidebarNavItem({ item, activeSubMenu, toggleSubMenu, ind
         </span>
 
         <span>
-          {activeSubMenu === index ? <FaChevronUp /> : <FaChevronDown />}
+          {open && activeIndex === index ? <FaChevronUp /> : <FaChevronDown />}
         </span>
       </button>
-      {activeSubMenu === index && (
-        <ul className="pl-4 text-black bg-[#f2f2f2] transition-transform duration-300 ease-in-out transform">
-          {item.subItems.map((subItem, subIndex) => (
-            <li key={subIndex} className="py-2 pl-8">
-              <Link to={subItem.path} className="">
-                {subItem.name}
-              </Link>
-            </li>
-          ))}
-        </ul>
-      )}
-    </li>
+
+      <div   ref={devRef}
+      style={{ height: open && activeIndex === index ? `${heights}px` : '0px' }}
+            className="pl-4 text-black bg-[#f2f2f2] transition-all duration-500 ease-in-out transform overflow-hidden"
+      >
+        {open && activeIndex === index && (
+          <ul
+            ref={devRef}
+            style={{ height: open && activeIndex === index ? `${heights}px` : '0px' }}
+            className="pl-4 text-black bg-[#f2f2f2] transition-all duration-500 ease-in-out transform overflow-hidden"
+          >
+            {item.subItems.map((subItem, subIndex) => (
+              <li key={subIndex} className="py-2 pl-8">
+                <Link to={subItem.path} className="">
+                  {subItem.name}
+                </Link>
+              </li>
+            ))}
+          </ul>
+        )}
+      </div>
+    </div>
   );
 }
+// export default function SidebarNavItem({ item,  index ,setOpen,open,activeIndex,setActiveIndex}) {
+//   const devRef = useRef()
+//   const[heights,setHeights]=useState(0)
+
+//   useEffect(()=>{
+//     if(devRef.current){
+//       setHeights(devRef.current.scrollHeight)
+
+//     }
+//     if(open===false){
+//       setActiveIndex(null)
+//     }
+//   },[heights,setOpen,open,index])
+
+
+ 
+//   return (
+//     <div key={index}>
+//       <button
+//       onClick={()=>{setOpen(!open),setActiveIndex(index)}}
+//         className="text-white w-full text-left py-1 px-3 flex justify-between items-center"
+       
+//       >
+//         <span className="flex items-center">
+//           {item.icon} {/* Icon displayed here */}
+//           <span className="ml-2">{item.name}</span>
+//         </span>
+
+//         <span>
+//           {""? <FaChevronUp /> : <FaChevronDown />}
+//         </span>
+//       </button>
+//       <div  className="">
+
+//       {open && activeIndex===index && (
+//         <ul ref={devRef} style={{height:open && activeIndex===index?`${heights}px`:'0px'  }} className={`  pl-4 text-black bg-[#f2f2f2] transition-all duration-500 ease-in-out transform`} >
+//           {item.subItems.map((subItem, subIndex) => (
+//             <li key={subIndex} className="py-2 pl-8">
+//               <Link to={subItem.path} className="">
+//                 {subItem.name}
+//               </Link>
+//             </li>
+//           ))}
+//         </ul>
+//       )}
+//       </div>
+//     </div>
+//   );
+// }
+
+
+// console.log(open)
+// console.log(activeIndex)
+
+// console.log(index)
+
+// console.log(activeIndex===index)
